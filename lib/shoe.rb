fd3900e83f633573cbe2c147d8760b693107c57a
Yevgeniy <yevgeniyerot@gmail.com>
2019-03-12 12:42:02 -0400

    Finished shoe class

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(shoe)
+    @shoe = shoe
+    @brand = "Nike"
+  end
+
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
